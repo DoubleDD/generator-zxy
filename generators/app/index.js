4f744a031413f95786d16073cfde5da63530fc55
Double_DD <1150744690@qq.com>
2019-09-04 22:33:54 +0800

    done

--- generators/app/index.js
+++ generators/app/index.js
@@ -22,27 +22,33 @@ module.exports = class extends Generator {
         this.appName = 'demo';
         // 用户名
         this.user = userInfo.username;
+        this.subdirs = [
+            'demo-api',
+            'demo-async-service',
+            'demo-service',
+            'demo-web-server'
+        ];
     }
 
     async prompting() {
         this.settings = await this.prompt([{
-                type: "input",
-                name: "project",
-                message: "Your project name",
-                default: this.project // 项目路径
-            },
-            {
-                type: "input",
-                name: "package",
-                message: "Your project package",
-                default: this.package // Default to current package name
-            },
-            {
-                type: "input",
-                name: "user",
-                message: "Your username",
-                default: this.user
-            }
+            type: "input",
+            name: "project",
+            message: "Your project name",
+            default: this.project // 项目路径
+        },
+        {
+            type: "input",
+            name: "package",
+            message: "Your project package",
+            default: this.package // Default to current package name
+        },
+        {
+            type: "input",
+            name: "user",
+            message: "Your username",
+            default: this.user
+        }
         ]);
     }
 
@@ -64,20 +70,10 @@ module.exports = class extends Generator {
         // step1.获取demo文件夹下的非目录文件，将非目录文件拷贝出来
         this._private_step1(sourceRoot, targetRoot, data)
 
-        // step2.copy demo-api 文件夹
-        this._private_step2(path.normalize(sourceRoot + '/demo-api'), targetRoot, data)
-
-        // step3.copy demo-async-service 文件夹
-
-        // step4.copy demo-service 文件夹
-
-        // step5.copy demo-web-server 文件夹
-
-
-
-
-
-        // this.fs.copyTpl(source, target, module);
+        this.subdirs.forEach(name => {
+            let suffix = name.replace('demo', '');
+            this._private_step3(path.normalize(sourceRoot + '/' + name), suffix, targetRoot, data)
+        });
     }
 
     /**
@@ -86,26 +82,22 @@ module.exports = class extends Generator {
      */
     _private_step1(source, target, data) {
         this.log('step1.将非目录文件拷贝出来')
-        this.log('source=' + source)
 
         fs.readdir(source, (err, files) => {
             if (err) {
                 return console.error(err);
             }
 
-            files.forEach(file => fs.stat(path.normalize(source + '/' + file), (err, stats) => {
-                if (err) {
-                    return console.error(err);
+            files.forEach(file => {
+                if (file.indexOf('demo-') > -1) {
+                    return;
                 }
-
+                // 将文件拷贝出来
                 let sourceFilePaht = path.normalize(source + '/' + file);
-                if (stats.isFile()) {
-                    // 将文件拷贝出来
-                    let targetFile = path.normalize(target + '/' + file);
-                    this.log(targetFile)
-                    this.fs.copyTpl(sourceFilePaht, targetFile, data);
-                }
-            }))
+                let targetFile = path.normalize(target + '/' + file);
+                this.fs.copyTpl(sourceFilePaht, targetFile, data);
+            })
+            this.log('step1.将非目录文件拷贝出来 done!')
         })
     }
 
@@ -115,87 +107,45 @@ module.exports = class extends Generator {
      * @param {*} target 
      * @param {*} data 
      */
-    _private_step2(source, target, data) {
-        this.log('step2.copy demo-api 文件夹')
-        this.log('source=' + source)
+    _private_step3(source, suffix, target, data) {
+        this.log(`step2.copy ${source} 文件夹`)
 
         // 创建 api 目录
-        let api_path = path.normalize(target + '/' + data.project + '-api');
+        let api_path = path.normalize(target + '/' + data.project + suffix);
         let src_path = path.normalize(api_path + '/src');
         let main_path = path.normalize(src_path + '/main');
         let test_path = path.normalize(src_path + '/test');
         let package_path = data.package.split('.').join('/');
-        this.log(package_path);
-        // 包路径
+
+        // 包路径 mian文件夹
         let main_package_path = path.normalize(main_path + '/java/' + package_path);
-        // 包路径
-        let test_package_path = path.normalize(test_path + '/java/' + package_path);
 
         // 复制 /src/main/java/demo/ 下的文件
         let source_main_package_path = path.normalize(source + '/src/main/java/demo');
+        this.log('拷贝main目录：' + source_main_package_path)
         this.fs.copyTpl(source_main_package_path, main_package_path, data);
 
-        // 复制 /src/pom.xml 下的文件
-        let source_pom = path.normalize(source + '/src/pom.xml');
-        this.fs.copyTpl(source_pom, src_path, data);
-
-        mkdirp(test_package_path, (err) => {
-            if (err) throw err;
-        });
-    }
-
-
-    /**
-     * step2.copy demo-api 文件夹
-     * @param {*} source 
-     * @param {*} target 
-     * @param {*} data 
-     */
-    _private_step2(source, target, data) {
-        this.log('step2.copy demo-api 文件夹')
-        this.log('source=' + source)
-
-        // 创建 api 目录
-        let api_path = path.normalize(target + '/' + data.project + '-api');
-        let src_path = path.normalize(api_path + '/src');
-        let main_path = path.normalize(src_path + '/main');
-        let test_path = path.normalize(src_path + '/test');
-        let package_path = data.package.split('.').join('/');
-        this.log(package_path);
-        // 包路径
-        let main_package_path = path.normalize(main_path + '/java/' + package_path);
-        // 包路径
-        let test_package_path = path.normalize(test_path + '/java/' + package_path);
-
-        // 复制 /src/main/java/demo/ 下的文件
-        let source_main_package_path = path.normalize(source + '/src/main/java/demo');
-        this.fs.copyTpl(source_main_package_path, main_package_path, data);
-
-        // 复制 /src/ 下的其他文件
-        let source_src = path.normalize(source + '/src');
-        fs.readdir(source_src, (err, files) => {
+        // 复制 /demo-api/ 下的其他文件
+        fs.readdir(source, (err, files) => {
             if (err) {
                 return console.error(err);
             }
 
-            files.forEach(file => fs.stat(path.normalize(source_src + '/' + file), (err, stats) => {
-                if (err) {
-                    return console.error(err);
+            files.forEach(file => {
+                if (file != 'src') {
+                    let sourceFilePaht = path.normalize(source + '/' + file);
+                    this.log('复制src并级文件：' + file + '->' + src_path)
+                    this.fs.copyTpl(sourceFilePaht, path.normalize(src_path + '/' + file), data);
                 }
-
-                let sourceFilePaht = path.normalize(source_src + '/' + file);
-                if (stats.isFile()) {
-                    // 将文件拷贝出来
-                    let targetFile = path.normalize(source_src + '/' + file);
-                    this.log(targetFile)
-                    this.fs.copyTpl(sourceFilePaht, targetFile, data);
-                }
-            }))
+            })
         })
 
+
+        // 包路径 test文件夹
+        let test_package_path = path.normalize(test_path + '/java/' + package_path);
+        this.log('创建测试目录：' + test_package_path)
         mkdirp(test_package_path, (err) => {
             if (err) throw err;
         });
     }
-
 };
